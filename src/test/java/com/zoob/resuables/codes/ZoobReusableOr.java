9fe5dd4f255e651de5f0617e1ab8df69335bb528
qashailesh08 <qa.shailesh08@gmail.com>
2025-05-27 22:05:09 +0530

    widely common useable code of ZoobShop

--- src/test/java/com/zoob/resuables/codes/ZoobReusableOr.java
+++ src/test/java/com/zoob/resuables/codes/ZoobReusableOr.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.zoob.webutils.WebUtil;
 
+import lombok.Getter;
+
+@Getter
 public class ZoobReusableOr {
 
 	public ZoobReusableOr(WebUtil driver) {
@@ -20,11 +23,62 @@ public class ZoobReusableOr {
 
 	@FindBy(xpath = "//a[text()=' Contact Us']")
 	protected WebElement contactLT;
-	
+
 	@FindBy(xpath = "//a[text()='Add a listing']")
 	protected WebElement addListLT;
-	
+
 	@FindBy(xpath = "//ul[@class='mobile-sub wsmenu-list']//child::a[text()='Sports']")
 	protected WebElement sportsLT;
+
+	@FindBy(xpath = "//ul[@class='mobile-sub wsmenu-list']//a[text()='Directory']")
+	private WebElement directoryLT;
+
+	@FindBy(xpath = "//ul[@class='mobile-sub wsmenu-list']//a[text()='Events']")
+	private WebElement eventsLT;
+
+	@FindBy(xpath = "//ul[@class='mobile-sub wsmenu-list']//a[text()='Classifieds']")
+	private WebElement classifiedsLT;
+
+	@FindBy(xpath = "//ul[@class='mobile-sub wsmenu-list']//a[text()='Jobs']")
+	private WebElement jobsLT;
+
+	@FindBy(xpath = "//ul[@class='mobile-sub wsmenu-list']//a[text()='Real Estate']")
+	private WebElement realEstateLT;
+
+	@FindBy(xpath = "//ul[@class='mobile-sub wsmenu-list']//a[text()='Autos']")
+	private WebElement autosLT;
+
+	@FindBy(xpath = "//ul[@class='mobile-sub wsmenu-list']//a[text()='News']")
+	private WebElement newsLT;
+
+	@FindBy(xpath = "//input[@id='searchtext']")
+	private WebElement searchBarTB;
+
+	@FindBy(xpath = "//input[@id='searchtext']/parent::div//button[@type='submit']")
+	private WebElement searchSubmitBT;
+	
+	@FindBy(xpath = "//a[text()='Advanced Search']")
+	private WebElement advancedSearchLT;
+
+	@FindBy(xpath = "//input[@id='search' and @required='required']")
+	private WebElement cmnSearchTB;
+	
+	@FindBy(xpath = "//ul[@id='search_nav']//button")
+	private WebElement cmnSearchButton;
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/test/java/com/zoob/resuables/codes/ZoobReusablePage.java
+++ src/test/java/com/zoob/resuables/codes/ZoobReusablePage.java
@@ -1,5 +1,84 @@
 package com.zoob.resuables.codes;
 
-public class ZoobReusablePage {
+import com.zoob.webutils.WebUtil;
+
+public class ZoobReusablePage extends ZoobReusableOr {
+
+	private WebUtil util;
+
+	public ZoobReusablePage(WebUtil util) {
+		super(util);
+		this.util = util;
+	}
+
+	public void signUpBT() {
+		util.click(getSignUpBT(), "Sign Up Button");
+	}
+
+	public void loginBT() {
+		util.click(getLoginBT(), "Login Button");
+	}
+
+	public void contactUsLT() {
+		util.click(getContactLT(), "Constact US Link");
+	}
+
+	public void addListingLT() {
+		util.click(getAddListLT(), "Add a Listing Link");
+	}
+
+	public void sportLT() {
+		util.click(getSportsLT(), "Sport Link");
+	}
+
+	public void directoryLT() {
+		util.click(getDirectoryLT(), "Directory Link");
+	}
+
+	public void eventsLT() {
+		util.click(getEventsLT(), "Events Link");
+	}
+
+	public void classifiedsLT() {
+		util.click(getClassifiedsLT(), "Classifields Link");
+	}
+
+	public void jobsLT() {
+		util.click(getJobsLT(), "Job Link");
+	}
+
+	public void realEstateLT() {
+		util.click(getRealEstateLT(), "Real Estate Link");
+	}
+
+	public void autosLT() {
+		util.click(getAutosLT(), "Autos Link");
+	}
+
+	public void newsLT() {
+		util.click(getNewsLT(), "News Link");
+	}
+
+	public void searchBarTB(String searchValue) {
+		util.clear(getSearchBarTB(), "Search Bas Text Box");
+		util.inputValue(getSearchBarTB(), searchValue, "Search Bas Text Box");
+	}
+
+	public void searchSubmitBT() {
+		util.click(getSearchSubmitBT(), "Search Submit Button");
+	}
+
+	public void advancedSearchLT() {
+		util.click(getAdvancedSearchLT(), "Advanced SearchLT");
+	}
+
+	public void cmnSearchBar(String searchValue) {
+		util.clear(getCmnSearchTB(), "Common Search Text Bar");
+		util.inputValue(getCmnSearchTB(), searchValue, "Common Search Text Bar");
+	}
+
+	public void cmnSubmitButton() {
+		util.click(getCmnSearchButton(), "Common Search Button");
+	}
 
 }
